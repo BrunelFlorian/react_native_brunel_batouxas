86005bda9fc898be01f12daf753ccc69ab45753c
Florian Brunel <bru.flo@hotmail.fr>
2023-06-12 10:44:31 +0200

    Add category stub

--- data/stub.ts
+++ data/stub.ts
@@ -1,5 +1,6 @@
 import { ListProduct } from "../models/ListProduct";
 import { Product } from "../models/Product";
+import { Category } from "../models/Category";
 
 const PRODUCT_DATA: Product[] = [
   new Product("id1prod", "Apple", "Description Apple", "Fruits" ),
@@ -12,3 +13,9 @@ export const LISTPRODUCT_DATA: ListProduct[] = [
   new ListProduct("id3","Meat", []),
 ];
 
+// Add product category stub
+export const LISTCATEGORY_DATA: Category[] = [
+  new Category("1", "Fruits"),
+  new Category("2", "Vegetables"),
+  new Category("3", "Meat"),
+]
